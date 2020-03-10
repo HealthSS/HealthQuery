48c668d5b87cd8d810c04f42e90c5211aa7ad005
Karandeep <kahluwa1@binghamton.edu>
2020-03-10 08:52:12 -0400

    server file changes

--- server/server.js
+++ server/server.js
@@ -1,29 +1,5 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
+const app = require("./app.js");
 const port = 3000;
-
-app.use(express.json());
-app.use(express.urlencoded());
-
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, //add path to static file here))
-})
-
-
-app.get('/coroni', (req, res) => {
-  res.status(200).json('everybody run');
-});
-
-app.post('/coroni', (req, res) => {
-  res.status(200).json('updated coronis')
-});
-
-app.use('*', (req, res) => {
-  res.sendStatus(404);
-});
-
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
