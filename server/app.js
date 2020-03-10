8acc379c9a7958d6c1f5ad13838c7c3865959c4d
Karandeep <kahluwa1@binghamton.edu>
2020-03-10 11:03:32 -0400

    Fixed server files

--- server/app.js
+++ server/app.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
-const coroniController = require('./coroniController')
+const coroniController = require("./coroniController");
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, //add path to static file here))
-})
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
 
-app.get('/coroni', coroniController.getData, (req, res) => {
+app.get("/coroni", coroniController.getData, (req, res) => {
   res.status(200).json(res.locals.getData);
 });
 
-app.post('/coroni', coroniController.updateTask, (req, res) => {
-  res.status(200).json('updated coronis database')
+app.post("/coroni", coroniController.updateData, (req, res) => {
+  res.status(200).json("updated coronis database");
 });
 
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'an express error occurred, this is a global error',
+    log: "an express error occurred, this is a global error",
     status: 400,
-    message: { err: 'internal error has occurred' }
-  }
-  const newErr = Object.assign(defaultErr, err)
-  res.status(newErr.status).json(newErr.message)
-})
+    message: { err: "internal error has occurred" }
+  };
+  const newErr = Object.assign(defaultErr, err);
+  res.status(newErr.status).json(newErr.message);
+});
 
-app.use('*', (req, res) => {
+app.use("*", (req, res) => {
   res.sendStatus(404);
 });
 
