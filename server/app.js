8b25c8d777cc6d15c523b72e017b52d9c9d52d03
Karandeep <kahluwa1@binghamton.edu>
2020-03-10 11:03:32 -0400

    Fixed server files

--- server/app.js
+++ server/app.js
@@ -1,27 +1,36 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const port = 3000;
+const coroniController = require("./coroniController");
 
 app.use(express.json());
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.sendFile(
-    path.resolve(
-      __dirname //add path to static file here))
-    )
-  );
+  res.sendStatus(200);
 });
 
-app.get("/coroni", (req, res) => {
-  res.status(200).json("everybody run");
+app.get("/coroni", coroniController.getData, (req, res) => {
+  res.status(200).json(res.locals.getData);
 });
 
-app.post("/coroni", (req, res) => {
-  res.status(200).json("updated coronis");
+app.post("/coroni", coroniController.updateData, (req, res) => {
+  res.status(200).json("updated coronis database");
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "an express error occurred, this is a global error",
+    status: 400,
+    message: { err: "internal error has occurred" }
+  };
+  const newErr = Object.assign(defaultErr, err);
+  res.status(newErr.status).json(newErr.message);
 });
 
 app.use("*", (req, res) => {
   res.sendStatus(404);
 });
+
+module.exports = app;
--- server/database.js
+++ server/database.js
@@ -1,17 +1,18 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const pg_uri = 'postgres://iykfwlmg:87mJp0PTZSwWBf9eWLcmh8QwTDPbgL77@drona.db.elephantsql.com:5432/iykfwlmg';
+const pg_uri =
+  "postgres://iykfwlmg:87mJp0PTZSwWBf9eWLcmh8QwTDPbgL77@drona.db.elephantsql.com:5432/iykfwlmg";
 
 const pool = new Pool({
-  connectionString: pg_uri;
-})
+  connectionString: pg_uri
+});
 
-const queryString = 'CREATE TABLE IF NOT EXISTS cases (id SERIAL PRIMARY KEY, city VARCHAR(255), confirmed INTEGER, deaths INTEGER, recovered INTEGER, active INTEGER)';
-
+const queryString =
+  "CREATE TABLE IF NOT EXISTS cases (id SERIAL PRIMARY KEY, city VARCHAR(255), confirmed INTEGER, deaths INTEGER, recovered INTEGER, active INTEGER)";
 
 module.exports = {
   query: (text, params, callback) => {
     console.log(`Executed query ${text}`);
-    return pool.query(text, params, callback)
+    return pool.query(text, params, callback);
   }
-}
+};
