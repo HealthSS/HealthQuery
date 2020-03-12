8121a5771a6e1632c4e7f7d0b186dc4323088acb
Josh <Joshua-Howard@users.noreply.github.com>
2020-03-12 18:37:02 -0400

    Add Pie Charts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,43 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Login from './loginPage.jsx'
-import AdminPage from './AdminPage.jsx'
-import Map from './Map.jsx'
-import Container from './container.jsx'
-import { useDispatch, useSelector } from "react-redux";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Login from './loginPage.jsx';
+import AdminPage from './AdminPage.jsx';
+import Map from './Map.jsx';
+import Container from './container.jsx';
+import { useDispatch, useSelector } from 'react-redux';
 import PieChartContainer from './PieChartContainer.jsx';
 
-
-
 export default function App() {
-
   // const loggedState = useSelector(state => state.isLoggedIn)
-  
-  const loggedState = "user";
+
+  const loggedState = 'user';
 
   return (
     <Router>
-        <Route exact path="/" component={()=>{
-          if (loggedState === "Admin") {return <AdminLogIn/>}
-          else if (loggedState === 'user') { return <LoggedIn/>}
-          else {return <Home/>}
-          }} />
+      <Route
+        exact
+        path="/"
+        component={() => {
+          if (loggedState === 'Admin') {
+            return <AdminLogIn />;
+          } else if (loggedState === 'user') {
+            return <LoggedIn />;
+          } else {
+            return <Home />;
+          }
+        }}
+      />
     </Router>
-
   );
 }
 function Home() {
   return (
     <div id="login">
-      <div id='loginSection'>
-        <Login/>
+      <div id="loginSection">
+        <Login />
       </div>
-         {/* <Map/> */}
-         <PieChartContainer/>
+      {/* <Map/> */}
+      <PieChartContainer />
     </div>
   );
 }
@@ -46,18 +45,19 @@ function Home() {
 function LoggedIn() {
   return (
     <div>
-      <Map/>
-      <Container/>
+      <Map />
+      <Container />
+      <PieChartContainer />
     </div>
   );
 }
 
-
 function AdminLogIn() {
   return (
     <div>
-      <Map/>
-      <AdminPage/>
+      <Map />
+      <AdminPage />
+      <PieChartContainer />
     </div>
   );
 }
